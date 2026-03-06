e163ebff3be294c6775d5be43df11a1ea1637c85
Dean Kamali <dean@stackblaze.com>
2026-03-06 18:43:55 +0000

    retrigger preview v5

--- server.js
+++ server.js
@@ -10,3 +10,4 @@ app.get('/health', (req, res) => res.json({ status: 'ok', version: '2.0' }))
 // preview retrigger v2
 // preview retrigger v3
 // preview retrigger v4
+// preview retrigger v5 - fixed templates
