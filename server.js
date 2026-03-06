3bb032472e348da9781f9548875d19e36fece540
Dean Kamali <dean@stackblaze.com>
2026-03-06 18:31:04 +0000

    retrigger preview v2

--- server.js
+++ server.js
@@ -7,3 +7,4 @@ console.log('Server running at http://127.0.0.1:1337/');
 // Health endpoint
 app.get('/health', (req, res) => res.json({ status: 'ok', version: '2.0' }))
 // retrigger
+// preview retrigger v2
