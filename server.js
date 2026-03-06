5f39ee78be02309159945a5f8a7dc304671379cf
Dean Kamali <dean@stackblaze.com>
2026-03-06 17:33:21 +0000

    add /health endpoint

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ http.createServer(function (req, res) {
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
+// Health endpoint
+app.get('/health', (req, res) => res.json({ status: 'ok', version: '2.0' }))
